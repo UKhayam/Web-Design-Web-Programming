76cf09654e330be8a91ae5929d167d2dd631f021
Umar13755 <umarkhayyam570@gmail.com>
2024-12-16 21:41:15 +0500

    lab 03 added

---
+++ lab/lab 3/script.js
@@ -0,0 +1,36 @@
+
+function (){
+    // Function
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function handleClick(){
+//     let number = document.getElementById("number").value;
+    
+    
+//    if(number === ""){
+//     alert("Please Enter a Number...!");
+//    }
+//    else{
+//     if(number % 2 === 0){
+//         alert("The Number is Even");
+//     }
+//     else{
+//         alert("The Number is ODD");
+//     }
+//    }
+// }
